657637b925a427eec40f4870b379719ba9687609
Kael <>
2025-05-02 17:25:09 +0300

    add tests for app

---
+++ app/test/test_home.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+
+url = "http://127.0.0.1:8080"
+
+def test_home():
+    path = "/"
+    request_url = url + path
+    data = requests.get(request_url)
+    assert data.status_code == 200
+    assert data.content == b'"Hello docker!"'
